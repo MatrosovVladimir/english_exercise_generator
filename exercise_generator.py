24c07a9f1a930e2140d78f0f839f720b239e1a0d
Matrosov Vladimir <morris73@mail.ru>
2023-07-08 17:03:07 +0300

    first commit

---
+++ exercise_generator.py
@@ -0,0 +1,415 @@
+import pandas as pd
+import spacy
+import string
+
+from nltk.tokenize import sent_tokenize
+from googletrans import Translator
+from word_forms.word_forms import get_word_forms
+
+import random
+import gensim.downloader as api
+
+import requests
+
+# Предобученная модель spacy
+nlp = spacy.load("en_core_web_sm")
+
+# Предобученная модель word2vec-ruscorpora-300 c gensim
+modelru = api.load("word2vec-ruscorpora-300")
+
+
+class TextToDataFrame:
+    def __init__(self, text):
+        """
+        Конструктор класса TextToDataFrame.
+
+        Аргументы:
+        text - текст для обработки.
+        """
+        self.text = text
+
+    def txt_to_df(self):
+        """
+        Метод для преобразования текста в датафрейм.
+
+        Возвращает:
+        Датафрейм, содержащий предложения из текста и дополнительные столбцы
+        для упражнений по английскому языку.
+        """
+        # Разбиваем текст на предложения
+        sentences = sent_tokenize(self.text)
+
+        # Удаляем пробелы и табуляции в начале и в конце каждого предложения
+        sentences = [sentence.strip() for sentence in sentences]
+
+        # Создаем датафрейм из списка предложений
+        df = pd.DataFrame(sentences, columns=['sentence'])
+
+        # Добавляем новые столбцы в датафрейм
+        df['exercise_type'] = ''
+        df['exercise_sentence'] = ''
+        df['options'] = ''
+        df['correct_answer'] = ''
+
+        return df
+
+
+class DictionaryCreator:
+    def __init__(self, api_key):
+        """
+        Конструктор класса DictionaryCreator.
+        
+        Аргументы:
+        api_key - API-ключ для доступа к сервису Yandex Dictionary.
+        """
+        self.api_key = api_key
+    
+    def get_transcription(self, word):
+        """
+        Метод для получения транскрипции слова с помощью API сервиса Yandex Dictionary.
+        
+        Аргументы:
+        word - слово для транскрипции.
+        
+        Возвращает:
+        Транскрипцию слова или None, если транскрипция не найдена.
+        """
+        # URL для запроса к API сервиса Yandex Dictionary
+        url = f"https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key={self.api_key}&lang=en-ru&text={word}"
+
+        # Отправляем запрос к API сервиса Yandex Dictionary
+        response = requests.get(url)
+
+        # Проверяем статус ответа
+        if response.status_code == 200:
+            # Получаем JSON-ответ
+            data = response.json()
+
+            # Получаем список элементов с транскрипциями
+            transcription_elements = data.get('def', [])
+
+            # Проверяем, найден ли хотя бы один элемент
+            if len(transcription_elements) > 0:
+                # Получаем первый элемент списка
+                transcription_element = transcription_elements[0]
+
+                # Получаем текст элемента
+                transcription = transcription_element.get('ts')
+
+                return transcription
+            else:
+                # Если элемент не найден, возвращаем None
+                return None
+        else:
+            # Если статус ответа не равен 200, возвращаем None
+            return None
+    
+    def create_dictionary_df(self, df, min_word_length=6):
+        """
+        Метод для создания датафрейма со словами, их транскрипциями и переводами из другого датафрейма.
+        
+        Аргументы:
+        df - датафрейм с предложениями для анализа.
+        min_word_length - минимальная длина слова, слова короче этой длины не будут включены в датафрейм.
+        
+        Возвращает:
+        Датафрейм, где первая колонка - это сложные слова, вторая колонка - транскрипции слов, 
+        а третья колонка - переводы на русский язык.
+        """
+        # Создаем пустой список для хранения данных
+        data = []
+
+        # Создаем множество для хранения уникальных слов
+        unique_words = set()
+
+        # Создаем объект Translator
+        translator = Translator()
+
+        # Получаем список предложений из датафрейма
+        sentences = df['sentence'].tolist()
+
+        for sentence in sentences:
+            # Удаляем знаки препинания из предложения
+            sentence = sentence.translate(str.maketrans('', '', string.punctuation))
+
+            # Разбиваем предложение на слова
+            words = sentence.split()
+
+            for word in words:
+
+                # Определяем длину слова
+                word_length = len(word)
+
+                # Если длина слова больше или равна минимальной
+                if word_length >= min_word_length:
+
+                    # Проверяем, есть ли уже такое слово в множестве
+                    if word not in unique_words:
+
+                        # Добавляем слово в множество
+                        unique_words.add(word)
+
+                        # Получаем перевод слова
+                        translation = translator.translate(word, src='en', dest='ru').text
+
+                        # Получаем транскрипцию слова
+                        transcription = self.get_transcription(word)
+
+                        # Добавляем данные в список
+                        data.append((word.capitalize(), transcription, translation.capitalize()))
+
+        # Создаем датафрейм из списка данных
+        result_df = pd.DataFrame(data, columns=['Word', 'Transcription', 'Translation'])
+        result_df = result_df.dropna()
+        result_df = result_df.reset_index(drop=True)
+
+        return result_df
+
+
+class ExerciseGenerator:
+    def generate_translate_exercise(self, row):
+        """
+        Функция для генерации упражнения на перевод слова.
+    
+        Аргументы:
+        row - строка датафрейма, содержащая предложение и столбцы для упражнений.
+    
+        Возвращает:
+        Обновленную строку с информацией об упражнении на перевод слова.
+        """
+        # Получаем предложение
+        sentence = row['sentence']
+    
+        # Разбиваем предложение на токены с помощью spacy
+        doc = nlp(sentence)
+    
+        # Выбираем токены, которые являются существительными или прилагательными
+        tokens = [token for token in doc if token.pos_ in ['NOUN', 'ADJ']]
+    
+        # Если есть подходящие токены
+        if len(tokens) > 0:
+            # Выбираем случайный токен
+            token = random.choice(tokens)
+        
+            # Получаем текст токена
+            token_text = token.text
+        
+            # Выделяем выбранное слово жирным шрифтом
+            exercise_sentence = sentence.replace(token_text, f'**{token_text}**')
+        
+            # Проверяем, начинается ли слово с заглавной буквы
+            is_title = token_text.istitle()
+        
+            # Преобразуем слово в нижний регистр для поиска похожих слов в модели word2vec
+            token_text = token_text.lower()
+        
+            # Находим перевод выбранного слова
+            translator = Translator()
+            translation = translator.translate(token_text, src='en', dest='ru').text
+
+            try:
+                # Получаем ближайшие слова из модели word2vec
+                pos_map = {'NOUN': 'NOUN', 'ADJ': 'ADJ'}
+                key = f"{translation}_{pos_map[token.pos_]}"
+                similar_words = modelru.most_similar(key, topn=3)
+            
+                # Выбираем два ближайших слова из списка ближайших слов и удаляем приписки _NOUN и _ADJ
+                options = [word.split('_')[0] for word, similarity in similar_words 
+                           if word.endswith(pos_map[token.pos_]) and '::' not in word]
+
+                # Если слово начиналось с заглавной буквы, преобразуем перевод в заглавный
+                if is_title:
+                    options = [option.title() for option in options]
+                    translation = translation.title()
+                
+                # Добавляем перевод верного ответа в список вариантов
+                options.append(translation)
+            
+                # Удаляем дубликаты из списка вариантов
+                options = list(set(options))
+            
+                # Перемешиваем список вариантов
+                random.shuffle(options)
+            
+                # Обновляем строку с информацией об упражнении
+                row['exercise_type'] = 'Выберите перевод слова'
+                row['exercise_sentence'] = exercise_sentence
+                row['options'] = options
+                row['correct_answer'] = translation
+        
+            except KeyError:
+                # Если нет подходящих слов в модели, заполняем все поля None
+                row['exercise_type'] = None
+                row['exercise_sentence'] = None
+                row['options'] = None
+                row['correct_answer'] = None
+    
+        else:
+            # Если нет подходящих токенов, заполняем все поля None
+            row['exercise_type'] = None
+            row['exercise_sentence'] = None
+            row['options'] = None
+            row['correct_answer'] = None
+    
+        return row
+
+
+    def generate_verb_exercise(self, row):
+        """
+        Функция для генерации упражнения с глаголами.
+    
+        Аргументы:
+        row - строка датафрейма, содержащая предложение и столбцы для упражнений.
+    
+        Возвращает:
+        Обновленную строку с информацией об упражнении с глаголами.
+        """
+        # Получаем предложение
+        sentence = row['sentence']
+    
+        # Разбиваем предложение на токены с помощью spacy
+        doc = nlp(sentence)
+    
+        # Выбираем токены, которые являются глаголами
+        tokens = [token for token in doc if token.pos_ == 'VERB']
+    
+        # Если есть подходящие токены
+        if len(tokens) > 0:
+            # Выбираем случайный токен
+            token = random.choice(tokens)
+        
+            # Получаем текст токена
+            token_text = token.text
+        
+            # Заменяем токен на пропуск в предложении
+            start = token.idx
+            end = start + len(token)
+            exercise_sentence = sentence[:start] + '___' + sentence[end:]
+        
+            # Получаем верный ответ
+            correct_answer = token_text
+        
+            # Проверяем, начинается ли слово с заглавной буквы
+            is_title = token_text.istitle()
+        
+            # Преобразуем слово в нижний регистр для получения других форм глагола
+            token_text = token_text.lower()
+        
+            # Получаем другие формы глагола
+            word_forms = get_word_forms(token_text)
+        
+            # Получаем список форм глагола
+            verb_forms = list(word_forms['v'])
+
+            # Используем все формы глагола из списка
+            options = verb_forms
+        
+            # Если слово начиналось с заглавной буквы, преобразуем варианты в заглавные
+            if is_title:
+                options = [option.title() for option in options]
+        
+            # Перемешиваем список вариантов
+            random.shuffle(options)
+        
+            # Обновляем строку с информацией об упражнении
+            row['exercise_type'] = 'Выберите форму глагола'
+            row['exercise_sentence'] = exercise_sentence
+            row['options'] = options
+            row['correct_answer'] = correct_answer
+    
+        else:
+            # Если нет подходящих токенов, заполняем все поля None
+            row['exercise_type'] = None
+            row['exercise_sentence'] = None
+            row['options'] = None
+            row['correct_answer'] = None
+    
+        return row
+    
+    def generate_article_exercise(self, row):
+        """
+        Функция для генерации упражнения на употребление артиклей.
+    
+        Аргументы:
+        row - строка датафрейма, содержащая предложение и столбцы для упражнений.
+    
+        Возвращает:
+        Обновленную строку с информацией об упражнении на употребление артиклей.
+        """
+        # Получаем предложение
+        sentence = row['sentence']
+    
+        # Разбиваем предложение на токены с помощью spacy
+        doc = nlp(sentence)
+    
+        # Выбираем токены, которые являются артиклями
+        tokens = [token for token in doc if token.pos_ == 'DET' and token.text.lower() in ['a', 'an', 'the']]
+    
+        # Если есть подходящие токены
+        if len(tokens) > 0:
+            # Выбираем случайный токен
+            token = random.choice(tokens)
+        
+            # Получаем текст токена
+            token_text = token.text
+        
+            # Проверяем, начинается ли слово с заглавной буквы
+            is_title = token_text.istitle()
+        
+            # Заменяем токен на пропуск в предложении
+            start = token.idx
+            end = start + len(token)
+            exercise_sentence = sentence[:start] + '___' + sentence[end:]
+        
+            # Получаем верный ответ
+            correct_answer = token_text
+        
+            # Создаем список вариантов ответа
+            options = ['a', 'an', 'the']
+        
+            # Если слово начиналось с заглавной буквы, преобразуем варианты в заглавные
+            if is_title:
+                options = [option.title() for option in options]
+        
+            # Обновляем строку с информацией об упражнении
+            row['exercise_type'] = 'Выберите артикль'
+            row['exercise_sentence'] = exercise_sentence
+            row['options'] = options
+            row['correct_answer'] = correct_answer
+    
+        else:
+            # Если нет подходящих токенов, заполняем все поля None
+            row['exercise_type'] = None
+            row['exercise_sentence'] = None
+            row['options'] = None
+            row['correct_answer'] = None
+    
+        return row
+    
+    def generate_random_exercises(self, df):
+        """
+        Метод для генерации случайных упражнений.
+        
+        Аргументы:
+        df - датафрейм, содержащий предложения и столбцы для упражнений.
+        
+        Возвращает:
+        Обновленный датафрейм с информацией об упражнениях.
+        """
+        for i, row in df.iterrows():
+            # Выбираем случайную функцию генерации упражнений
+            exercise_function = random.choice([self.generate_translate_exercise,
+                                               self.generate_verb_exercise,
+                                               self.generate_article_exercise])
+
+            # Применяем функцию генерации упражнений к строке
+            row = exercise_function(row)
+
+            # Обновляем строку в датафрейме
+            df.iloc[i] = row
+
+        # Удаляем пустые строки и сбрасываем индексы
+        df = df.dropna()
+        df = df.reset_index(drop=True)
+
+        return df
---
+++ streamlit_app.py
@@ -0,0 +1,203 @@
+# Импортируем необходимые библиотеки
+import streamlit as st
+from PIL import Image
+from urllib.request import urlopen
+from io import BytesIO
+
+# Импортируем классы из файла exercise_generator
+from exercise_generator import TextToDataFrame, DictionaryCreator, ExerciseGenerator
+
+# Задаем ключ API для доступа к словарю
+api_key = 'dict.1.1.20230629T105549Z.fb37ebf7609e3a28.23abdd368ee0f4b1fe26c9f8d9b4d3097fab27a7'
+
+
+# Определяем функцию для загрузки данных из текста
+@st.cache_data
+def creating_a_dictionary_and_exercises(text):
+    """
+    Функция для загрузки данных из текста.
+
+    Аргументы:
+    text - текст для обработки.
+
+    Возвращает:
+    Кортеж из двух датафреймов:
+    - Датафрейм с упражнениями по английскому языку.
+    - Датафрейм со словарем.
+    """
+    # Создаем объект класса TextToDataFrame
+    text_to_df = TextToDataFrame(text)
+
+    # Преобразуем текст в датафрейм с упражнениями
+    english_exercises_df = text_to_df.txt_to_df()
+
+    # Создаем объект класса DictionaryCreator
+    dictionary_creator = DictionaryCreator(api_key)
+
+    # Создаем датафрейм со словарем
+    dictionary_df = dictionary_creator.create_dictionary_df(english_exercises_df)
+
+    # Создаем объект класса ExerciseGenerator
+    exercise_generator = ExerciseGenerator()
+
+    # Генерируем случайные упражнения
+    english_exercises_df = exercise_generator.generate_random_exercises(english_exercises_df)
+
+    # Возвращаем датафреймы с упражнениями и словарем
+    return english_exercises_df, dictionary_df
+
+
+# Создаем виджет для загрузки файлов
+uploaded_file = st.file_uploader("Загрузите текстовый файл", type=["txt"])
+
+# Проверяем, был ли загружен файл
+if uploaded_file is not None:
+    # Читаем содержимое файла и преобразуем его в строку
+    text = uploaded_file.read().decode()
+
+    # Загружаем данные из текста
+    english_exercises_df, dictionary_df = creating_a_dictionary_and_exercises(text)
+
+    # Отображаем изображение в шапке страницы
+    img_url = "https://climbingthedissertationmountain.files.wordpress.com/2018/01/cropped-vladislav-klapin-316711.jpg"
+    image_data = urlopen(img_url).read()
+    header_image = Image.open(BytesIO(image_data))
+    st.image(header_image, use_column_width=True)
+
+    # Отображаем датафрейм со словарем на странице
+    st.dataframe(dictionary_df, height=300, width=900)
+
+    # Получаем список типов упражнений из датафрейма с упражнениями
+    exercise_types = english_exercises_df["exercise_type"].unique().tolist()
+
+    # Создаем виджет для выбора типов упражнений в боковой панели
+    selected_types = st.sidebar.multiselect("Выберите типы упражнений", exercise_types)
+
+    # Фильтруем датафрейм с упражнениями по выбранным типам упражнений
+    if not selected_types:
+        filtered_df = english_exercises_df.copy()
+    else:
+        filtered_df = english_exercises_df[english_exercises_df["exercise_type"].isin(selected_types)]
+
+    # Получаем общее количество упражнений в отфильтрованном датафрейме
+    total_exercises = len(filtered_df)
+
+    # Получаем текущий индекс и текущий счет из состояния сессии (или задаем значения по умолчанию)
+    current_index = st.session_state.get("current_index", 0)
+    current_score = st.session_state.get("current_score", 0)
+
+    # Создаем виджет для отображения прогресса выполнения упражнений в боковой панели
+    progress_bar = st.sidebar.progress(current_score / total_exercises)
+
+    # Создаем пустой элемент для отображения текущего счета в боковой панели
+    score_text = st.sidebar.empty()
+
+    # Отображаем текущий счет в пустом элементе
+    score_text.text(f"Решено {current_score} из {total_exercises} упражнений")
+
+    # Отображаем 5 упражнений на странице
+    correct_answers = 0
+    for i in range(5):
+
+        # Проверяем, есть ли еще упражнения для отображения
+        if current_index + i < total_exercises:
+
+            # Отображаем номер упражнения
+            st.markdown(f"**Упражнение {current_index + i + 1}**")
+
+            # Получаем тип упражнения из датафрейма
+            exercise_type = filtered_df.iloc[current_index + i]["exercise_type"]
+
+            # Отображаем тип упражнения
+            st.write(f"Тип упражнения: {exercise_type}")
+
+            # Получаем предложение для упражнения из датафрейма
+            exercise_sentence = filtered_df.iloc[current_index + i]["exercise_sentence"]
+
+            # Выделяем пропущенное слово красным цветом
+            highlighted_sentence = exercise_sentence.replace("**",
+                                                             "<span style='border: 2px solid red;'>",
+                                                             1).replace("**", "</span>", 1)
+
+            # Отображаем предложение для упражнения с выделенным пропущенным словом
+            st.markdown(f"{highlighted_sentence}", unsafe_allow_html=True)
+
+            # Получаем варианты ответов из датафрейма
+            options = filtered_df.iloc[current_index + i]["options"]
+
+            # Добавляем пустой вариант ответа в начало списка
+            options.insert(0, "")
+
+            # Создаем выпадающий список с вариантами ответов
+            selectbox = st.selectbox(f"Выберите верный ответ", options, key=f"selectbox_{i}")
+
+            # Получаем правильный ответ из датафрейма
+            correct_answer = filtered_df.iloc[current_index + i]["correct_answer"]
+
+            # Проверяем, выбрал ли пользователь вариант ответа
+            if selectbox:
+
+                # Проверяем, верный ли выбранный вариант ответа
+                if selectbox == correct_answer:
+
+                    # Отображаем сообщение об успехе
+                    st.success("✅ Верно!")
+
+                    # Увеличиваем счет на 1
+                    current_score += 1
+
+                    # Обновляем прогресс выполнения упражнений
+                    progress_bar.progress(current_score / total_exercises)
+
+                    # Обновляем отображение текущего счета
+                    score_text.text(f"Решено {current_score} из {total_exercises} упражнений")
+
+                    # Увеличиваем счетчик правильных ответов на 1
+                    correct_answers += 1
+
+                else:
+                    # Отображаем сообщение об ошибке
+                    st.error("❌ Неверно!")
+
+                    # Отображаем правильный ответ
+                    st.write(f"Правильный ответ: {correct_answer}")
+
+        else:
+            break
+
+    # Проверяем, правильно ли решены все 5 упражнений на странице
+    if correct_answers == 5:
+        # Запускаем анимацию воздушных шаров
+        st.balloons()
+
+    # Проверяем, есть ли еще упражнения для отображения на следующей странице
+    if current_index + 5 < total_exercises:
+
+        # Создаем кнопку для перехода к следующим 5 упражнениям
+        if st.button("Следующие 5 упражнений"):
+            # Увеличиваем текущий индекс на 5
+            current_index += 5
+
+            # Сохраняем текущий индекс в состоянии сессии
+            st.session_state["current_index"] = current_index
+
+            # Сохраняем текущий счет в состоянии сессии
+            st.session_state["current_score"] = current_score
+
+            # Перезапускаем приложение
+            st.experimental_rerun()
+
+    else:
+        # Отображаем сообщение об окончании упражнений
+        st.write("Поздравляем! Вы решили все упражнения!")
+
+        # Отображаем итоговый счет
+        st.write(f"Ваш итоговый счет: {current_score} из {total_exercises}")
+
+        # Создаем кнопку для начала заново
+        if st.button("Начать заново"):
+            # Очищаем состояние сессии
+            st.session_state.clear()
+
+            # Перезапускаем приложение
+            st.experimental_rerun()
